459b14f9e474bc2b345717b2f8c19fca26e2b109
Clément DOUIN <soywod@users.noreply.github.com>
2019-09-06 01:57:43 +0200

    fix export script

--- utils/post-export.js
+++ utils/post-export.js
@@ -3,23 +3,20 @@ const fs = require('fs')
 const sitemap = require('sitemap')
 require('dotenv').config()
 
-const buildPaths = require('./path')
-
-function buildSitemapFile(paths) {
-  const urls = Object.keys(paths).map(path => ({
-    url: path,
+function buildSitemapFile() {
+  const url = {
+    url: '/',
     changefreq: 'monthly',
     priority: 1,
-  }))
+  }
 
   const config = {
     hostname: process.env.HOSTNAME,
     cacheTime: 3600,
-    urls,
+    urls: [url],
   }
 
   const xml = sitemap.createSitemap(config)
-
   fs.writeFileSync(path.resolve(__dirname, '../out/sitemap.xml'), xml.toString())
 }
 
@@ -31,7 +28,5 @@ function moveFiles(files) {
   }
 }
 
-buildPaths().then(paths => {
-  buildSitemapFile(paths)
-  moveFiles(['robots.txt', '_redirects'])
-})
+buildSitemapFile()
+moveFiles(['robots.txt', '_redirects'])
